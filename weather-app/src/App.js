5755cf7cd35bcdbb3aa33c5a65775bc7cb6bacb8
Dannzii Malcolm <you@example.com>
2020-04-25 00:31:49 -0500

    Added the buttons

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import NavBar from './components/NavBar'
 import './App.css';
+import ButtonSwitch from './components/ButtonSwitch'
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <ButtonSwitch/>
     </div>
   );
 }
--- weather-app/src/components/ButtonSwitch.js
+++ weather-app/src/components/ButtonSwitch.js
@@ -3,5 +3,35 @@ import Button from '@material-ui/core/Button';
 import ButtonGroup from '@material-ui/core/ButtonGroup';
 import { makeStyles } from '@material-ui/core/styles';
 
+const useStyles = makeStyles((theme) => ({
+    root: {
+      display: 'flex',
+      alignItems: 'right',
+      '& > *': {
+        margin: theme.spacing(10),
+      },
+    },
+  }));
 
 
+
+
+export default function Buttongroup(){
+    const Buttons = useStyles();
+
+    return(
+        <div className={Buttons.root}>
+            <ButtonGroup  variant="text" color="primary" aria-label="text primary button group">
+                <Button>
+                    Temprature
+                </Button>
+                <Button>
+                    Wind Speed
+                </Button>
+                <Button>
+                    Cloud Forecast
+                </Button>
+            </ButtonGroup>
+        </div>
+    );
+}
